25e37ee2bb35ade536d22981f6aa95953016eb5a
Junxiao Shi <git@mail1.yoursunny.com>
2020-06-21 07:22:06 -0400

    tlv: simplify NNI implementation

--- src/ndnph/tlv/nni.hpp
+++ src/ndnph/tlv/nni.hpp
@@ -7,122 +7,130 @@
 namespace ndnph {
 namespace tlv {
 
-/** @brief 1-byte number encoding. */
-class NNI1
+class NNI;
+
+namespace detail {
+
+template<typename T>
+class NNIValue
 {
 public:
-  static bool decode(const Decoder::Tlv& d, uint8_t& value)
+  static bool decode(const Decoder::Tlv& d, T& value)
   {
-    if (d.length != sizeof(value)) {
+    if (d.length != sizeof(T)) {
       return false;
     }
-    value = d.value[0];
+    value = readValue(d.value);
     return true;
   }
 
-  explicit NNI1(uint8_t value)
-    : m_value(value)
+  explicit NNIValue(T number)
+    : m_number(number)
   {}
 
   void encodeTo(Encoder& encoder) const
   {
-    uint8_t* room = encoder.prependRoom(sizeof(m_value));
+    uint8_t* room = encoder.prependRoom(sizeof(m_number));
     if (room != nullptr) {
-      room[0] = m_value;
+      writeValue(room, m_number);
     }
   }
 
 private:
-  uint8_t m_value = 0;
+  static T readValue(const uint8_t* value);
+
+  static void writeValue(uint8_t* room, T n);
+
+  friend NNI;
+
+private:
+  T m_number = 0;
 };
+
+template<>
+inline uint8_t
+NNIValue<uint8_t>::readValue(const uint8_t* value)
+{
+  return value[0];
+}
+
+template<>
+inline void
+NNIValue<uint8_t>::writeValue(uint8_t* room, uint8_t n)
+{
+  room[0] = n;
+}
+
+template<>
+inline uint16_t
+NNIValue<uint16_t>::readValue(const uint8_t* value)
+{
+  return (static_cast<uint16_t>(value[0]) << 8) | static_cast<uint16_t>(value[1]);
+}
+
+template<>
+inline void
+NNIValue<uint16_t>::writeValue(uint8_t* room, uint16_t n)
+{
+  room[0] = static_cast<uint8_t>(n >> 8);
+  room[1] = static_cast<uint8_t>(n);
+}
+
+template<>
+inline uint32_t
+NNIValue<uint32_t>::readValue(const uint8_t* value)
+{
+  return (static_cast<uint32_t>(value[0]) << 24) | (static_cast<uint32_t>(value[1]) << 16) |
+         (static_cast<uint32_t>(value[2]) << 8) | static_cast<uint32_t>(value[3]);
+}
+
+template<>
+inline void
+NNIValue<uint32_t>::writeValue(uint8_t* room, uint32_t n)
+{
+  room[0] = static_cast<uint8_t>(n >> 24);
+  room[1] = static_cast<uint8_t>(n >> 16);
+  room[2] = static_cast<uint8_t>(n >> 8);
+  room[3] = static_cast<uint8_t>(n);
+}
+
+template<>
+inline uint64_t
+NNIValue<uint64_t>::readValue(const uint8_t* value)
+{
+  return (static_cast<uint64_t>(value[0]) << 56) | (static_cast<uint64_t>(value[1]) << 48) |
+         (static_cast<uint64_t>(value[2]) << 40) | (static_cast<uint64_t>(value[3]) << 32) |
+         (static_cast<uint64_t>(value[4]) << 24) | (static_cast<uint64_t>(value[5]) << 16) |
+         (static_cast<uint64_t>(value[6]) << 8) | static_cast<uint64_t>(value[7]);
+}
+
+template<>
+inline void
+NNIValue<uint64_t>::writeValue(uint8_t* room, uint64_t n)
+{
+  room[0] = static_cast<uint8_t>(n >> 56);
+  room[1] = static_cast<uint8_t>(n >> 48);
+  room[2] = static_cast<uint8_t>(n >> 40);
+  room[3] = static_cast<uint8_t>(n >> 32);
+  room[4] = static_cast<uint8_t>(n >> 24);
+  room[5] = static_cast<uint8_t>(n >> 16);
+  room[6] = static_cast<uint8_t>(n >> 8);
+  room[7] = static_cast<uint8_t>(n);
+}
+
+} // namespace detail
+
+/** @brief 1-byte number encoding. */
+using NNI1 = detail::NNIValue<uint8_t>;
+
+/** @brief 2-byte number encoding. */
+using NNI2 = detail::NNIValue<uint16_t>;
 
 /** @brief 4-byte number encoding. */
-class NNI4
-{
-public:
-  static bool decode(const Decoder::Tlv& d, uint32_t& value)
-  {
-    if (d.length != sizeof(value)) {
-      return false;
-    }
-    value = readValue(d);
-    return true;
-  }
-
-  explicit NNI4(uint32_t value)
-    : m_value(value)
-  {}
-
-  void encodeTo(Encoder& encoder) const
-  {
-    uint8_t* room = encoder.prependRoom(sizeof(m_value));
-    if (room != nullptr) {
-      room[0] = m_value >> 24;
-      room[1] = m_value >> 16;
-      room[2] = m_value >> 8;
-      room[3] = m_value;
-    }
-  }
-
-private:
-  static uint32_t readValue(const Decoder::Tlv& d)
-  {
-    return (static_cast<uint32_t>(d.value[0]) << 24) | (static_cast<uint32_t>(d.value[1]) << 16) |
-           (static_cast<uint32_t>(d.value[2]) << 8) | d.value[3];
-  }
-
-  friend class NNI;
-
-private:
-  uint32_t m_value = 0;
-};
+using NNI4 = detail::NNIValue<uint32_t>;
 
 /** @brief 8-byte number encoding. */
-class NNI8
-{
-public:
-  static bool decode(const Decoder::Tlv& d, uint64_t& value)
-  {
-    if (d.length != sizeof(value)) {
-      return false;
-    }
-    value = readValue(d);
-    return true;
-  }
-
-  explicit NNI8(uint64_t value)
-    : m_value(value)
-  {}
-
-  void encodeTo(Encoder& encoder) const
-  {
-    uint8_t* room = encoder.prependRoom(sizeof(m_value));
-    if (room != nullptr) {
-      room[0] = m_value >> 56;
-      room[1] = m_value >> 48;
-      room[2] = m_value >> 40;
-      room[3] = m_value >> 32;
-      room[4] = m_value >> 24;
-      room[5] = m_value >> 16;
-      room[6] = m_value >> 8;
-      room[7] = m_value;
-    }
-  }
-
-private:
-  static uint64_t readValue(const Decoder::Tlv& d)
-  {
-    return (static_cast<uint64_t>(d.value[0]) << 56) | (static_cast<uint64_t>(d.value[1]) << 48) |
-           (static_cast<uint64_t>(d.value[2]) << 40) | (static_cast<uint64_t>(d.value[3]) << 32) |
-           (static_cast<uint64_t>(d.value[4]) << 24) | (static_cast<uint64_t>(d.value[5]) << 16) |
-           (static_cast<uint64_t>(d.value[6]) << 8) | d.value[7];
-  }
-
-  friend class NNI;
-
-private:
-  uint64_t m_value = 0;
-};
+using NNI8 = detail::NNIValue<uint64_t>;
 
 /** @brief NonNegativeInteger encoding. */
 class NNI
@@ -137,50 +145,46 @@ public:
                                                                 std::numeric_limits<I>>::type>
   static bool decode(const Decoder::Tlv& d, I& value, uint64_t max = Limit::max())
   {
-    uint64_t v = 0;
+    uint64_t n = 0;
     switch (d.length) {
       case 1:
-        v = d.value[0];
+        n = NNI1::readValue(d.value);
         break;
       case 2:
-        v = (static_cast<uint16_t>(d.value[0]) << 8) | d.value[1];
+        n = NNI2::readValue(d.value);
         break;
       case 4:
-        v = NNI4::readValue(d);
+        n = NNI4::readValue(d.value);
         break;
       case 8:
-        v = NNI8::readValue(d);
+        n = NNI8::readValue(d.value);
         break;
       default:
         return false;
     }
-    value = v;
-    return v <= max;
+    value = n;
+    return n <= max;
   }
 
-  explicit NNI(uint64_t value)
-    : m_value(value)
+  explicit NNI(uint64_t number)
+    : m_number(number)
   {}
 
   void encodeTo(Encoder& encoder) const
   {
-    if (m_value <= std::numeric_limits<uint8_t>::max()) {
-      NNI1(m_value).encodeTo(encoder);
-    } else if (m_value <= std::numeric_limits<uint16_t>::max()) {
-      uint8_t* room = encoder.prependRoom(sizeof(uint16_t));
-      if (room != nullptr) {
-        room[0] = m_value >> 8;
-        room[1] = m_value;
-      }
-    } else if (m_value <= std::numeric_limits<uint32_t>::max()) {
-      NNI4(m_value).encodeTo(encoder);
+    if (m_number <= std::numeric_limits<uint8_t>::max()) {
+      NNI1(m_number).encodeTo(encoder);
+    } else if (m_number <= std::numeric_limits<uint16_t>::max()) {
+      NNI2(m_number).encodeTo(encoder);
+    } else if (m_number <= std::numeric_limits<uint32_t>::max()) {
+      NNI4(m_number).encodeTo(encoder);
     } else {
-      NNI8(m_value).encodeTo(encoder);
+      NNI8(m_number).encodeTo(encoder);
     }
   }
 
 private:
-  uint64_t m_value = 0;
+  uint64_t m_number = 0;
 };
 
 } // namespace tlv
