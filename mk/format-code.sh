88cbc4299bc1b5af9f56b972dc93729e4c57294c
Junxiao Shi <git@mail1.yoursunny.com>
2021-08-27 00:41:27 +0000

    mk: clang-8 => clang-11

--- mk/format-code.sh
+++ mk/format-code.sh
@@ -3,4 +3,4 @@ set -eo pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")"/..
 
 git ls-files '*.hpp' '*.cpp' '*.inc' '*.ino' | \
-  xargs clang-format-8 -i -style=file
+  xargs clang-format-11 -i -style=file
